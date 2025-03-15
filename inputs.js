94f07e1f27a9841084678ec89edbc6765ad645d3
nikkiaah <nikki_aoe@myabcusd.org>
2025-03-15 14:26:11 -0700

    Update inputs.js

--- inputs.js
+++ inputs.js
@@ -4,11 +4,18 @@
 //let weight = 125;
 //let height = 62;
 
-//getting inputs from html elements
-const age = document.getElementById("age")
-let gender = document.getElementById("gender")
-const weight = document.getElementById("weight")
-const height = document.getElementById("height")
+//getting inputs from html elements (and dropdowns too)
+const age = document.getElementById("age");
+
+let genderdropdown = document.getElementById("gender");
+let genderValue = genderdropdown.getValue;
+
+const weight = document.getElementById("weight");
+
+const height = document.getElementById("height");
+
+let FreqExerciseDropdown = document.getElementById("");
+let FreqExercise = document.getValue;
 
 
 // Amount of exercise affects the number of calories
@@ -25,9 +32,9 @@ if (FreqExercise === 1) {
 
 // FoodToday object to store food data
 let FoodToday = {
-  Name: [],
-  Category: [],
-  Calories: []
+  Name: [], //name of food
+  TimeOfDay: [], //breakfast, lunch, dinner, or snack
+  Calories: [] //straightforward
 };
 
 // Food class
@@ -39,7 +46,7 @@ class Food {
 
     // Adding to the FoodToday dictionary
     FoodToday.Name.push(this.name);
-    FoodToday.Category.push(this.category);
+    FoodToday.TimeOfDay.push(this.category);
     FoodToday.Calories.push(this.calories);
   }
 }
@@ -47,9 +54,16 @@ class Food {
 // Creating a new food object
 let potato = new Food("Potato", "Carbs", 600);
 
-// Summing up the calories
+// Displaying the data as a table
+console.table(FoodToday);
+
+//total summary of number of calories
 const sumCalories = FoodToday.Calories.reduce((acc, curr) => acc + curr, 0);
 console.log("Total Calories: " + sumCalories);
 
-// Displaying the data as a table
-console.table(FoodToday);
+if (sumCalories >= KiloCalories){
+  console.log("Congratulations on meeting your calorie goal today!");
+} else{
+  console.log("Keep on going! You can reach your goal!");
+}
+  
